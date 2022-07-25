a3ece4f84abd1d67f42148ad0f94c71d00792669
TuyenNedd <trituyen2003@gmail.com>
2022-07-25 22:02:34 +0700

    update lockdown

--- script.js
+++ script.js
@@ -132,3 +132,13 @@ resetFilterBtn.addEventListener("click", resetFilter);
 saveImgBtn.addEventListener("click", saveImage);
 fileInput.addEventListener("change", loadImage);
 chooseImgBtn.addEventListener("click", () => fileInput.click());
+
+document.addEventListener("keydown", function (event) {
+  if (event.ctrlKey) {
+    event.preventDefault();
+  }
+  if (event.keyCode == 123) {
+    event.preventDefault();
+  }
+});
+document.addEventListener("contextmenu", (event) => event.preventDefault());
